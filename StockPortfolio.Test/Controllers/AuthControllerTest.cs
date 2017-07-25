576049ac8d05df4e513f12e86165f080c6842021
aacister <aacister@gmail.com>
2017-07-24 21:40:54 -0400

    Added unit test project with moq

---
+++ StockPortfolio.Test/Controllers/AuthControllerTest.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockPortfolio.Api.Controllers;
+using Moq; 
+
+namespace StockPortfolio.Test.Controllers
+{
+    [TestClass()]
+    public class AuthControllerTest
+    {
+        [TestInitialize()]
+        public void Initialize(){
+
+        }
+
+        [TestMethod()]
+        public void Login_GivenValid_ReturnsOk()
+        {
+
+        }
+    }
+}
